7de48eba9f482bc4d4892b8fc9f4ec5d5749599c
paraches <iphonedevjpyatta@gmail.com>
2012-02-24 00:26:10 +0900

    Initial Commit

---
+++ SimulatorCharCase/IDYViewController.h
@@ -0,0 +1,13 @@
+//
+//  IDYViewController.h
+//  SimulatorCharCase
+//
+//  Created by 手代木 伸一 on 12/02/24.
+//  Copyright (c) 2012年 paraches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDYViewController : UIViewController
+
+@end
